143df98f6a79d0111564a90c50612a37cc029d97
mfyz <fatih@mfyz.com>
2014-06-20 00:18:03 -0400

    finished first simple version of the app.

---
+++ platforms/ios/www/js/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('deviceready', function(){
+	console.log('Device Ready!');
+});
+
+weatherApp = angular.module('WeatherApp', ['ngRoute']);
+
+weatherApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/weather', {
+			templateUrl: 'views/weather.html',
+			controller:  'WeatherController'
+		}).
+		when('/settings', {
+			templateUrl: 'views/settings.html',
+			controller:  'SettingsController'
+		}).
+		otherwise({
+			redirectTo: '/weather'
+		});
+}]);
+
+weatherApp.controller('WeatherController', ['$scope', '$http', function($scope, $http){
+	$scope.ready = false;
+
+	apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=New%20York,NY';
+
+	$http({
+		url: apiUrl,
+		method: 'GET'
+	}).success(function(data, status, headers, config) {
+		console.log(data);
+		$scope.ready = true;
+		$scope.degree = parseInt(data.main.temp - 273.15);
+		$scope.weather = data.weather[0];
+	});
+}]);
